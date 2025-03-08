c8da8e8d982f16fa73cc2cf25dba199ed95f243a
moha-salad64 <mohamedsalaad509@gmail.com>
2025-03-09 00:24:27 +0300

    creating routing pages

--- MERN-AUTH/src/App.jsx
+++ MERN-AUTH/src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import {BrowserRouter , Routes, Route, Router} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SingIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 export default function App() {
   return (
-    <div className='text-red- text-center'>App</div>
+    
+    <BrowserRouter>
+    <Routes>
+    <Route path ="/" element={<Home />}/>
+    <Route path="/about" element={< About/>}/>
+    <Route path="/profile" element={<Profile/>}/>
+    <Route path="/signin" element={<SingIn/>}/>
+    <Route path="/signup" element={<SignUp/>}/>
+    </Routes>
+    </BrowserRouter>
+
+   
   )
 }
